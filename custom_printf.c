aebe5284deb80c9e4b6d04418b6ca7a680c78d51
Igbineweka <kelvin.igbineweka@yahoo.com>
2023-09-17 14:27:16 +0100

    handling format specifiers c, s, and % edited

--- custom_printf.c
+++ custom_printf.c
@@ -9,7 +9,7 @@
 
 int _printf(const char *format, ...)
 {
-	int x, char_count = 0, y;
+	int x, char_count = 0, y, spec_found = 0;
 	va_list params;
 /* create array of fmt struct, match specifier to corresponding function */
 	fmt specs_arr[] = {{"c", p_c}, {"s", p_str}, {"%", p_per}, {NULL, NULL}};
@@ -19,22 +19,28 @@ int _printf(const char *format, ...)
 		return (-1);
 	for (x = 0; format[x] != '\0'; x++)
 	{
-		if (format[x] != '%')/* check if zero directive is passed and print string */
+		/* check if zero directive is pass and print string */
+		if (format[x] != '%' || format[x + 1] == '\0')
 		{
 			_putchar(format[x]);
 			char_count++;
 		}
 		else /* handles various specifiers encoutered */
+		{
 			for (y = 0; *(specs_arr[y].specs) != '\0'; y++)
 			{
 				if (format[x + 1] == *(specs_arr[y].specs))
 				{
 				/* call the corresponding function in the arrays of function */
-					specs_arr[y].ptf(params);
+					char_count += specs_arr[y].ptf(params);
 					x++;
+					spec_found++;
 					break;
 				}
 			}
+			if (!spec_found) /* if no match is found print % to output */
+				_putchar('%');
+		}
 	}
 	va_end(params);
 	return (char_count);
